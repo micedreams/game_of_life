989207534801f3e1f9c700493fe72850f53de2e1
Aku <micedreams@Akus-MacBook-Air.local>
2020-12-12 01:05:22 +0530

    reset function

--- lib/home.dart
+++ lib/home.dart
@@ -40,6 +40,11 @@ class _HomeState extends State<Home> {
     world.setValues(arr);
   }
 
+  reset() {
+    Navigator.of(context).pop();
+    Navigator.of(context).pushNamed(Home.routeName);
+  }
+
   @override
   Widget build(context) {
     Stream<GameOfLife> _bids = (() async* {
@@ -49,38 +54,35 @@ class _HomeState extends State<Home> {
     })();
 
     return StreamBuilder<GameOfLife>(
-      stream: _bids,
-      builder: (context, snapshot) {
-        return new Scaffold(
-          appBar: new AppBar(
-            title: new Text("Game of life"),
-          ),
-          body: new Container(
-          child: GridView.count(
-            crossAxisCount: rows,
-            children: List.generate(total, (index) {
-              return new GridTile(
-                child: new Card(
-                  color: Colors.blue.shade200,
-                  child: Center(
-                    child: RaisedButton(
+        stream: _bids,
+        builder: (context, snapshot) {
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text("Game of life"),
+            ),
+            body: new Container(
+                child: GridView.count(
+              crossAxisCount: rows,
+              children: List.generate(total, (index) {
+                return new GridTile(
+                  child: new Card(
                       color: Colors.blue.shade200,
-                      onPressed: () {
-                        setValues();
-                        arr[index] = "X";
-                      },
-                      onLongPress: () {
-                        Navigator.of(context).pop();
-                        Navigator.of(context).pushNamed(Home.routeName);
-                      },
-                      child: Text(arr[index]),
-                    )
-                  )
-                ),
-              );
-            }),
-          )),
-        );
-      });
+                      child: Center(
+                          child: RaisedButton(
+                        color: Colors.blue.shade200,
+                        onPressed: () {
+                          setValues();
+                          arr[index] = "X";
+                        },
+                        onLongPress: () {
+                          reset();
+                        },
+                        child: Text(arr[index]),
+                      ))),
+                );
+              }),
+            )),
+          );
+        });
   }
 }
