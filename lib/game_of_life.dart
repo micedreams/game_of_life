43842d34a99875e75303bebdc7709176fd042673
Aku <micedreams@micedreams.domain.name>
2023-07-14 12:46:33 +0530

    Moving code around.

--- lib/game_of_life.dart
+++ lib/game_of_life.dart
@@ -1,55 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:game_of_life/game_of_life_bloc.dart';
-import 'package:game_of_life/game_of_life_event.dart';
-import 'package:game_of_life/game_of_life_state.dart';
+
+import 'game_of_life_bloc.dart';
+import 'game_of_life_event.dart';
+import 'game_of_life_state.dart';
 
 class GameOfLife extends StatefulWidget {
   const GameOfLife({super.key});
 
   @override
-  State<StatefulWidget> createState() => _GameOfLifeState();
+  State createState() => _GameOfLifeState();
 }
 
 class _GameOfLifeState extends State<GameOfLife> {
-  late GameOfLifeBloc bloc;
-
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
+  Widget build(context) => BlocProvider<GameOfLifeBloc>(
+        create: (context) => GameOfLifeBloc()..add(ResetEvent()),
+        child: BlocBuilder<GameOfLifeBloc, GameOfLifeState>(
+          builder: (context, state) {
+            final bloc = BlocProvider.of<GameOfLifeBloc>(context);
 
-    bloc = BlocProvider.of<GameOfLifeBloc>(context);
-  }
+            final arr = state.arr;
+            final total = arr.length;
+            final row = (total * 0.1).floor();
 
-  @override
-  Widget build(context) => BlocBuilder<GameOfLifeBloc, GameOfLifeState>(
-        builder: (context, state) {
-          final arr = state.arr;
-          final total = arr.length;
-          final row = (total * 0.1).floor();
-
-          return FutureBuilder(
-            future: setValues(bloc),
-            builder: (context, snapshot) => Scaffold(
-              body: GridView.count(
-                crossAxisCount: row,
-                children: List.generate(
-                  total,
-                  (index) => InkWell(
-                    onTap: () => bloc.add(ClickEvent(index)),
-                    onLongPress: () => bloc.add(ResetEvent()),
-                    child: 'X' == arr[index]
-                        ? const Card(
-                            color: Color.fromARGB(255, 54, 81, 24),
-                            elevation: 2.0,
-                          )
-                        : const Card(color: Color.fromARGB(255, 239, 194, 194)),
+            return FutureBuilder(
+              future: setValues(bloc),
+              builder: (context, snapshot) => Scaffold(
+                body: GridView.count(
+                  crossAxisCount: row,
+                  children: List.generate(
+                    total,
+                    (index) => InkWell(
+                      onTap: () => bloc.add(ClickEvent(index)),
+                      onLongPress: () => bloc.add(ResetEvent()),
+                      child: 'X' == arr[index]
+                          ? const Card(
+                              color: Color.fromARGB(255, 54, 81, 24),
+                              elevation: 2.0,
+                            )
+                          : const Card(
+                              color: Color.fromARGB(255, 239, 194, 194)),
+                    ),
                   ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       );
 
   Future<void> setValues(GameOfLifeBloc bloc) async => Future.delayed(
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:game_of_life/game_of_life_bloc.dart';
-import 'package:game_of_life/game_of_life_event.dart';
-import 'package:game_of_life/game_of_life.dart';
+
+import 'game_of_life.dart';
+
 
 void main() async {
   runApp(const GOL());
@@ -13,13 +12,10 @@ class GOL extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "Game of life",
       debugShowCheckedModeBanner: false,
-      home: BlocProvider<GameOfLifeBloc>(
-        create: (context) => GameOfLifeBloc()..add(ResetEvent()),
-        child: const GameOfLife(),
-      ),
+      home: GameOfLife(),
     );
   }
 }
