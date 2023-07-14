e2bb42f9d8f0413d5e7e692f5dd48c2388e7906a
Aku <micedreams@micedreams.domain.name>
2023-07-14 19:15:09 +0530

    Fix assertion.

--- lib/game_of_life_view.dart
+++ lib/game_of_life_view.dart
@@ -20,27 +20,29 @@ class GameOfLifeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: set,
-      builder: (context, snapshot) => Scaffold(
-        body: GridView.count(
-          crossAxisCount: row,
-          children: List.generate(
-            total,
-            (index) {
-              return InkWell(
-                onTap: () => onTap(index),
-                onLongPress: () => reset(index),
-                child: Card(
-                  color: condition(index)
-                      ? const Color.fromARGB(154, 54, 81, 24)
-                      : const Color.fromARGB(146, 135, 98, 111),
+    return (row <= 0 || total <= 0)
+        ? const SizedBox.shrink()
+        : FutureBuilder(
+            future: set,
+            builder: (context, snapshot) => Scaffold(
+              body: GridView.count(
+                crossAxisCount: row,
+                children: List.generate(
+                  total,
+                  (index) {
+                    return InkWell(
+                      onTap: () => onTap(index),
+                      onLongPress: () => reset(index),
+                      child: Card(
+                        color: condition(index)
+                            ? const Color.fromARGB(154, 54, 81, 24)
+                            : const Color.fromARGB(146, 135, 98, 111),
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
-          ),
-        ),
-      ),
-    );
+              ),
+            ),
+          );
   }
 }
